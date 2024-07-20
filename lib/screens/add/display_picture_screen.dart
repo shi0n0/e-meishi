4d81c1d47ae23f2f9946822077808d3465055375
shi0n0 <shiono.yui1080@gmail.com>
2024-07-20 13:44:15 +0900

    スワイプで戻るのを禁止し、ボタンの位置を軽く調整

--- lib/screens/add/display_picture_screen.dart
+++ lib/screens/add/display_picture_screen.dart
@@ -7,19 +7,38 @@ class DisplayPictureScreen extends StatelessWidget {
   final String imagePath;
 
   @override
-  Widget build(
-    BuildContext context,
-  ) {
-    return Stack(
-      children: [
-        Center(child: Image.file(File(imagePath))),
-        ElevatedButton(
-          onPressed: () {
-            context.pop(); // 撮り直しボタンを押すとカメラ画面に戻る
-          },
-          child: const Text('撮り直し'),
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (bool didPop) async {
+        // falseを返すことでスワイプで戻る機能を無効化
+        return Future.value();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('撮影した写真'),
+          automaticallyImplyLeading: false,
         ),
-      ],
+        body: Stack(
+          children: [
+            Image.file(File(imagePath)),
+            Positioned(
+              bottom: 20, // 画像の下からの距離を指定
+              left: 0,
+              right: 0,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: ElevatedButton(
+                  onPressed: () {
+                    context.pop(); // 撮り直しボタンを押すとカメラ画面に戻る
+                  },
+                  child: const Text('撮り直し'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
