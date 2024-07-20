e1672335e9baddddf4b5eabbb67c3327a93c09ce
shi0n0 <shiono.yui1080@gmail.com>
2024-07-20 12:49:00 +0900

    プレビューの上に取り直しボタンを配置

--- lib/screens/add/display_picture_screen.dart
+++ lib/screens/add/display_picture_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class DisplayPictureScreen extends StatelessWidget {
   const DisplayPictureScreen({super.key, required this.imagePath});
@@ -9,6 +10,16 @@ class DisplayPictureScreen extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
-    return Image.file(File(imagePath));
+    return Stack(
+      children: [
+        Center(child: Image.file(File(imagePath))),
+        ElevatedButton(
+          onPressed: () {
+            context.pop(); // 撮り直しボタンを押すとカメラ画面に戻る
+          },
+          child: const Text('撮り直し'),
+        ),
+      ],
+    );
   }
 }
