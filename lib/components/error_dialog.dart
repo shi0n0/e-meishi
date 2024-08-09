6707715bbd12282bb58978f896085c937ee734e8
shi0n0 <shiono.yui1080@gmail.com>
2024-08-09 21:53:25 +0900

    エラーダイアログのUIを作成

---
+++ lib/components/error_dialog.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class ErrorDialog extends StatelessWidget {
+  final String errorMessage;
+
+  const ErrorDialog({super.key, required this.errorMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('❌', style: TextStyle(fontSize: 40)),
+            Text('エラーが発生しました:$errorMessage')
+          ],
+        ),
+      ),
+    );
+  }
+}
