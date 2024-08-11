a729a46d0d1de53a1244e11e7e51c946d5a48ce5
shi0n0 <shiono.yui1080@gmail.com>
2024-08-11 23:48:34 +0900

    閉じるボタンを追加

--- lib/components/check_dialog.dart
+++ lib/components/check_dialog.dart
@@ -7,14 +7,20 @@ class CheckDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return Dialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: const Padding(
-        padding: EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('✔️', style: TextStyle(fontSize: 40)),
-            Text('処理が完了しました')
+            const Text('✔️', style: TextStyle(fontSize: 40)),
+            const Text('処理が完了しました'),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context, rootNavigator: true).pop();
+              },
+              child: const Text('閉じる'),
+            )
           ],
         ),
       ),
