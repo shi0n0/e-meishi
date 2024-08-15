a0923cdf90e0709f85a56393326e222bc6409e4b
shi0n0 <shiono.yui1080@gmail.com>
2024-08-15 22:57:37 +0900

    autoIncrementは重複できないためint型で定義を修正

--- lib/models/my_meishi_history.dart
+++ lib/models/my_meishi_history.dart
@@ -5,7 +5,8 @@ part 'my_meishi_history.g.dart';
 @collection
 class MyMeishiHistory {
   Id id = Isar.autoIncrement;
+
   final meishiId = 1;
-  final version = Isar.autoIncrement;
+  late int version;
   late String imagePath;
 }
