5aeb9e83cbc2936b1c7ca17f789c5b9212ea5371
shi0n0 <shiono.yui1080@gmail.com>
2024-08-17 23:06:45 +0900

    MyPageScreenクラスを定義

---
+++ lib/screens/my_page/my_page_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyPageScreen extends StatelessWidget {
+  const MyPageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyPageScreen(),
+    );
+  }
+}
