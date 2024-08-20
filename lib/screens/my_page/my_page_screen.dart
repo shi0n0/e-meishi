53344985b7603a8f233b1d4c933d7b6d753aea98
shi0n0 <shiono.yui1080@gmail.com>
2024-08-20 23:40:09 +0900

    MyMeishiViewを設置して自分の名刺を表示

--- lib/screens/my_page/my_page_screen.dart
+++ lib/screens/my_page/my_page_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:e_meishi/components/my_meishi_view.dart';
 
 class MyPageScreen extends StatelessWidget {
   const MyPageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyPageScreen(),
+    return const MyMeishiView(
+      meishiId: 1,
     );
   }
 }
