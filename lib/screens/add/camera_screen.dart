be410b4ca0c27496900e7ed9cf356d4b1327bdec
shi0n0 <shiono.yui1080@gmail.com>
2024-08-21 18:48:30 +0900

    カメラのガイドと撮影する区間を見た目上で制限させるUIを追加

--- lib/screens/add/camera_screen.dart
+++ lib/screens/add/camera_screen.dart
@@ -1,7 +1,9 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:e_meishi/components/capture_button.dart';
+import 'dart:ui';
 
 class CameraScreen extends StatefulWidget {
   final CameraDescription camera;
@@ -47,18 +49,61 @@ class _CameraScreenState extends State<CameraScreen> {
             }
           },
         ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 60),
-          child: Align(
-              alignment: Alignment.bottomCenter,
-              child: CaptureButton(onPressed: () async {
-                final image = await _controller.takePicture();
-                if (context.mounted) {
-                  context.push(
-                      '/display_picture?imageName=${Uri.encodeComponent(image.name)}&imagePath=${Uri.encodeComponent(image.path)}');
-                }
-              })),
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          top: MediaQuery.of(context).size.height / 2,
+          child: ClipRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              child: Container(
+                color: Colors.black87,
+                alignment: Alignment.topCenter, // 背景の暗さ
+                padding: const EdgeInsets.only(top: 20),
+                child: const Column(children: [
+                  Text(
+                    '上のガイドに従って名刺を撮影してください',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10), // 魔を追加
+
+                  Text(
+                    // 新しいテキストウィジェットを追加
+                    '影のできない環境で撮影すると\n綺麗な仕上がりになります',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ]),
+              ),
+            ),
+          ),
         ),
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            margin: const EdgeInsets.only(top: 100),
+            width: 300,
+            height: 200,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 3)),
+          ),
+        ),
+        Padding(
+            padding: const EdgeInsets.only(bottom: 60),
+            child: Align(
+                alignment: Alignment.bottomCenter,
+                child: CaptureButton(onPressed: () async {
+                  final image = await _controller.takePicture();
+                  if (context.mounted) {
+                    context.push(
+                        '/display_picture?imageName=${Uri.encodeComponent(image.name)}&imagePath=${Uri.encodeComponent(image.path)}');
+                  }
+                }))),
       ],
     );
   }
