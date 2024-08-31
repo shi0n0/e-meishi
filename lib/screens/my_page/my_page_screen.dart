d8f9c65a73d811868c427d121913420a16eea264
shi0n0 <shiono.yui1080@gmail.com>
2024-08-31 17:15:51 +0900

    transitionButtonを設置

--- lib/screens/my_page/my_page_screen.dart
+++ lib/screens/my_page/my_page_screen.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:e_meishi/components/my_meishi_view.dart';
+import 'package:e_meishi/components/transition_button.dart';
 
 class MyPageScreen extends StatelessWidget {
   const MyPageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MyMeishiView(
-      meishiId: 1,
+    return Scaffold(
+      // Scaffoldで画面全体を構成
+      appBar: AppBar(
+        title: const Text('マイページ'),
+      ),
+      body: const SingleChildScrollView(
+        child: Column(
+          // Columnでウィジェットを縦に並べる
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            MyMeishiView(
+              meishiId: 1,
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TransitionButton(
+                buttonText: '過去の名刺を見る',
+                transtion: '/history',
+                icon: Icons.history,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
