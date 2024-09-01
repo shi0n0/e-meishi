fb2bf77dd3819c18eeca62ff47bb54173c167a54
shi0n0 <shiono.yui1080@gmail.com>
2024-09-01 17:40:54 +0900

    不要なimportを削除

--- lib/screens/Introduction/introduction_screen.dart
+++ lib/screens/Introduction/introduction_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
