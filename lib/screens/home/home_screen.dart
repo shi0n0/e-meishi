06f4bb77803a219df5187049f8b0166ee4047a3d
shi0n0 <shiono.yui1080@gmail.com>
2024-09-01 22:05:30 +0900

    リクエストカードを設置

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:e_meishi/components/big_meishi_view.dart';
 import 'package:e_meishi/components/add_meishi_button.dart';
+import 'package:e_meishi/components/request.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,6 +14,7 @@ class HomeScreen extends StatelessWidget {
           Column(
             children: [
               BigMeishiView(meishiId: 1), //仮にmeishiId : 1を指定
+              Request()
             ],
           ),
           Positioned(
