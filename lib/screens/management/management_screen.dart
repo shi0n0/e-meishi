7f9425a02e496549db4964513b44fe85a7e7c407
shi0n0 <shiono.yui1080@gmail.com>
2024-09-01 17:09:39 +0900

    BottomNavigationBar実装の為、ページを作成

---
+++ lib/screens/management/management_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ManagementScreen extends StatelessWidget {
+  const ManagementScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('名刺管理ページ');
+  }
+}
