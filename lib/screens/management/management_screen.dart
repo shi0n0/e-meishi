6f89fc8d1c127504462218ba996f5a0a37dbdeaf
shi0n0 <shiono.yui1080@gmail.com>
2024-09-02 15:24:45 +0900

    AppBarに検索バーを設置

--- lib/screens/management/management_screen.dart
+++ lib/screens/management/management_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:e_meishi/components/search_field.dart';
 
 class ManagementScreen extends StatelessWidget {
   const ManagementScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('名刺管理ページ');
+    return Scaffold(
+      appBar: AppBar(
+        title: const SearchField(),
+      ),
+    );
   }
 }
