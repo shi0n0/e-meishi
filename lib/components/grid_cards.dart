0538bd5e52bb1a636e39e08c9d05c19e83196877
shi0n0 <shiono.yui1080@gmail.com>
2024-09-03 15:32:27 +0900

    独自スクロールの可否をパラメータで操作できるように変更

--- lib/components/grid_cards.dart
+++ lib/components/grid_cards.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 
 class GridCards extends StatelessWidget {
   final List list;
-  const GridCards({super.key, required this.list});
+  final bool isScrollable;
+  const GridCards({super.key, required this.list, this.isScrollable = true});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: GridView.builder(
+          shrinkWrap: isScrollable,
+          physics: isScrollable
+              ? const AlwaysScrollableScrollPhysics()
+              : const NeverScrollableScrollPhysics(),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               mainAxisSpacing: 8,
