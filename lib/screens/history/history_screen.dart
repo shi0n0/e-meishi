05bb3f00ebdc6467441a709f391120d4f550a255
shi0n0 <shiono.yui1080@gmail.com>
2024-09-03 17:55:55 +0900

    過去の名刺をgridCardsで追加

--- lib/screens/history/history_screen.dart
+++ lib/screens/history/history_screen.dart
@@ -5,31 +5,32 @@ import 'package:flutter/material.dart';
 class HistoryScreen extends StatelessWidget {
   HistoryScreen({super.key});
 
-  final List<String> list = [
-    'https://placehold.jp/380x240.png',
-    'https://placehold.jp/360x230.png',
-    'https://placehold.jp/340x220.png',
-    'https://placehold.jp/320x210.png',
-    'https://placehold.jp/300x200.png',
-    'https://placehold.jp/280x190.png',
-    'https://placehold.jp/260x180.png',
-    'https://placehold.jp/240x170.png',
-    'https://placehold.jp/220x160.png',
-    'https://placehold.jp/200x150.png'
+  final List<Map<String, String>> list = [
+    {'imageUrl': 'https://placehold.jp/380x240.png', 'addedTime': '2024-09-01'},
+    {'imageUrl': 'https://placehold.jp/360x230.png', 'addedTime': '2024-08-30'},
+    {'imageUrl': 'https://placehold.jp/340x220.png', 'addedTime': '2024-08-28'},
+    {'imageUrl': 'https://placehold.jp/320x210.png', 'addedTime': '2024-08-25'},
+    {'imageUrl': 'https://placehold.jp/300x200.png', 'addedTime': '2024-08-20'},
+    {'imageUrl': 'https://placehold.jp/280x190.png', 'addedTime': '2024-08-18'},
+    {'imageUrl': 'https://placehold.jp/260x180.png', 'addedTime': '2024-08-15'},
+    {'imageUrl': 'https://placehold.jp/240x170.png', 'addedTime': '2024-08-10'},
+    {'imageUrl': 'https://placehold.jp/220x160.png', 'addedTime': '2024-08-05'},
+    {'imageUrl': 'https://placehold.jp/200x150.png', 'addedTime': '2024-08-01'},
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('名刺履歴'),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              FittedBox(
+              const FittedBox(
                 child: Text(
                   '現在の名刺',
                   style: TextStyle(
@@ -39,13 +40,26 @@ class HistoryScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              BigMeishiView(meishiId: 1),
-              SizedBox(height: 8.0), // Dividerとのスペースを作る
-              Divider(
+              const BigMeishiView(meishiId: 1),
+              const SizedBox(height: 8.0), // Dividerとのスペースを作る
+              const Divider(
                 thickness: 1,
                 color: Colors.grey,
               ),
-
+              const FittedBox(
+                child: Text(
+                  '過去の名刺',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    fontFamily: 'Hiragino Sans',
+                  ),
+                ),
+              ),
+              GridCards(
+                list: list,
+                isScrollable: false,
+              )
             ],
           ),
         ),
