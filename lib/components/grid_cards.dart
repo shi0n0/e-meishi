4ddab47a4405ee169c6bc2dfec1345adcdb37215
shi0n0 <shiono.yui1080@gmail.com>
2024-09-03 17:26:30 +0900

    名刺の左上に登録日時を追加

--- lib/components/grid_cards.dart
+++ lib/components/grid_cards.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class GridCards extends StatelessWidget {
-  final List list;
+  final List<Map<String, String>> list;
   final bool isScrollable;
   const GridCards({super.key, required this.list, this.isScrollable = true});
 
@@ -21,12 +21,39 @@ class GridCards extends StatelessWidget {
               childAspectRatio: 8 / 5),
           itemCount: list.length,
           itemBuilder: (context, index) {
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.network(
-                list[index],
-                fit: BoxFit.cover,
-              ),
+            final imageUrl = list[index]['imageUrl'] ?? '';
+            final addedTime = list[index]['addedTime'] ?? '';
+
+            return Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    imageUrl,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: double.infinity,
+                  ),
+                ),
+                Positioned(
+                  top: 8,
+                  left: 8,
+                  child: Container(
+                    color: Colors.black.withOpacity(0.1), // 半透明の背景色
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 4.0, vertical: 2.0),
+                      child: Text(
+                        addedTime, // 動的に設定可能な登録日
+                        style: const TextStyle(
+                          fontSize: 10,
+                          color: Colors.white, // テキストを白色に
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             );
           }),
     );
