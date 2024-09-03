a49cdea68a47cab80e944b4e2a7c6b0234cdc1b2
shi0n0 <shiono.yui1080@gmail.com>
2024-09-03 15:27:11 +0900

    現在の名刺を設置

--- lib/screens/history/history_screen.dart
+++ lib/screens/history/history_screen.dart
@@ -1,10 +1,55 @@
+import 'package:e_meishi/components/big_meishi_view.dart';
+import 'package:e_meishi/components/grid_cards.dart';
 import 'package:flutter/material.dart';
 
 class HistoryScreen extends StatelessWidget {
-  const HistoryScreen({super.key});
+  HistoryScreen({super.key});
 
+  final List<String> list = [
+    'https://placehold.jp/380x240.png',
+    'https://placehold.jp/360x230.png',
+    'https://placehold.jp/340x220.png',
+    'https://placehold.jp/320x210.png',
+    'https://placehold.jp/300x200.png',
+    'https://placehold.jp/280x190.png',
+    'https://placehold.jp/260x180.png',
+    'https://placehold.jp/240x170.png',
+    'https://placehold.jp/220x160.png',
+    'https://placehold.jp/200x150.png'
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('名刺履歴'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FittedBox(
+                child: Text(
+                  '現在の名刺',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    fontFamily: 'Hiragino Sans',
+                  ),
+                ),
+              ),
+              BigMeishiView(meishiId: 1),
+              SizedBox(height: 8.0), // Dividerとのスペースを作る
+              Divider(
+                thickness: 1,
+                color: Colors.grey,
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
