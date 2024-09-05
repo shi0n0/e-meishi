5f616148af47a27cdd95f04f94db6d86af22e43b
shi0n0 <shiono.yui1080@gmail.com>
2024-09-05 23:51:35 +0900

    BigMeishiViewを設置

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -1,10 +1,13 @@
+import 'package:e_meishi/components/big_meishi_view.dart';
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+  final int meishiId;
+
+  const DetailScreen({super.key, required this.meishiId});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BigMeishiView(meishiId: meishiId);
   }
 }
