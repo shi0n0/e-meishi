a0576f7728b3d63c6724e6e9fe88238224186acc
shi0n0 <shiono.yui1080@gmail.com>
2024-09-14 23:38:10 +0900

    getRecent(新着順で取得)を作成

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,7 +1,10 @@
+import 'package:e_meishi/models/meishi.dart';
 import 'package:flutter/material.dart';
 import 'package:e_meishi/components/loading_dialog.dart';
 import 'package:e_meishi/components/check_dialog.dart';
 import 'package:e_meishi/components/error_dialog.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 void showLoadingDialog(BuildContext context, String message) {
   showDialog(
@@ -29,4 +32,16 @@ void showErrorDialog(BuildContext context, String errorMessage) {
       builder: (BuildContext context) {
         return ErrorDialog(errorMessage: errorMessage);
       });
+}
+
+/*DB関連*/
+// 新着順リスト形式で名刺を取得
+Future<List<Meishi>> getRecent() async {
+  final Isar? isar = Isar.getInstance(); // Isarインスタンスを取得
+  if (isar == null) {
+    return []; // Isarインスタンスがnullの場合、空のリストを返す
+  }
+  final List<Meishi> meishis =
+      await isar.meishis.where().sortByAddedTimeDesc().findAll(); // 名刺を新着順に取得
+  return meishis;
 }
