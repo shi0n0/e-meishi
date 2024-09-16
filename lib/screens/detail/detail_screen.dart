60950250782937aea8be58d0c4b296d466876b7f
shi0n0 <shiono.yui1080@gmail.com>
2024-09-16 23:00:37 +0900

    FutureBuilderで名刺データを取得し1行記述式コンポーネントに譲渡

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -2,6 +2,8 @@ import 'package:e_meishi/components/big_meishi_view.dart';
 import 'package:e_meishi/components/single_line_description.dart';
 import 'package:e_meishi/components/long_description.dart';
 import 'package:flutter/material.dart';
+import 'package:e_meishi/utils/utils.dart';
+import 'package:e_meishi/models/meishi.dart';
 
 class DetailScreen extends StatelessWidget {
   final int meishiId;
@@ -14,31 +16,65 @@ class DetailScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text(('名刺詳細ページ')),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              BigMeishiView(meishiId: meishiId),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
+        body: FutureBuilder<Meishi>(
+            future: getMeishiData(meishiId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                    child: CircularProgressIndicator()); // データ取得中のローディング表示
+              } else if (snapshot.hasError) {
+                return Center(
+                    child: Text('エラーが発生しました: ${snapshot.error}')); // エラーメッセージ表示
+              } else if (!snapshot.hasData || snapshot.data == null) {
+                return const Center(child: Text('データが見つかりません')); // データがない場合の表示
+              }
+
+              final meishiData = snapshot.data;
+
+              return SingleChildScrollView(
                 child: Column(
                   children: [
-                    OneLineDescription(hintText: '名前'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(child: OneLineDescription(hintText: '性別')),
-                        SizedBox(width: 8),
-                        Expanded(child: OneLineDescription(hintText: '年齢')),
-                      ],
-                    ),
-                    OneLineDescription(hintText: '所属'),
-                    OneLineDescription(hintText: '電話番号'),
-                    LongDescription(hintText: 'メモなど'),
+                    BigMeishiView(meishiId: meishiId),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          OneLineDescription(
+                            hintText: '名前',
+                            mainText: meishiData?.userName ?? '取得できませんでした',
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                  child: OneLineDescription(
+                                hintText: '性別',
+                                mainText: meishiData?.gender ?? '取得できませんでした',
+                              )),
+                              const SizedBox(width: 8),
+                              Expanded(
+                                  child: OneLineDescription(
+                                hintText: '年齢',
+                                mainText: meishiData?.age ?? '取得できませんでした',
+                              )),
+                            ],
+                          ),
+                          OneLineDescription(
+                            hintText: '所属',
+                            mainText: meishiData?.affiliation ??
+                                '取得できませんでした',
+                          ),
+                          OneLineDescription(
+                            hintText: '電話番号',
+                            mainText: meishiData?.phoneNumber ?? '取得できませんでした',
+                          ),
+                          const LongDescription(hintText: 'メモなど'),
+                        ],
+                      ),
+                    )
                   ],
                 ),
-              )
-            ],
-          ),
-        ));
+              );
+            }));
   }
 }
