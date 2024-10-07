79c996ec36a68d5f78ba64a65ae1ec54354f44ea
shi0n0 <shiono.yui1080@gmail.com>
2024-10-07 22:43:58 +0900

    settingページのテンプレートを作成

---
+++ lib/screens/setting/setting_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+    );
+  }
+}
