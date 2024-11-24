18cd5e53baa1c733406bd085b728c57a0066e3e4
shi0n0 <shiono.yui1080@gmail.com>
2024-11-24 22:00:33 +0900

    一般的な設定群を追加

--- lib/screens/setting/setting_screen.dart
+++ lib/screens/setting/setting_screen.dart
@@ -6,25 +6,71 @@ class SettingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SettingsList(
-      sections: [
-        SettingsSection(
-          title: const Text('Common'),
-          tiles: <SettingsTile>[
-            SettingsTile.navigation(
-              leading: const Icon(Icons.language),
-              title: const Text('Language'),
-              value: const Text('English'),
-            ),
-            SettingsTile.switchTile(
-              onToggle: (value) {},
-              initialValue: true,
-              leading: const Icon(Icons.format_paint),
-              title: const Text('Enable custom theme'),
-            ),
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '設定',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
         ),
-      ],
+      ),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: const Text('一般'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.language),
+                title: const Text('言語'),
+                value: const Text('日本語'),
+              ),
+              SettingsTile.switchTile(
+                onToggle: (value) {},
+                initialValue: true,
+                leading: const Icon(Icons.format_paint),
+                title: const Text('ダークテーマ'),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('データ'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.document_scanner),
+                title: const Text('利用規約'),
+              ),
+              SettingsTile.navigation(
+                leading: const Icon(Icons.privacy_tip),
+                title: const Text('プライバシーポリシー'),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('その他'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.question_answer),
+                title: const Text('よくある質問'),
+              ),
+              SettingsTile.navigation(
+                leading: const Icon(Icons.contact_support),
+                title: const Text('お問い合わせ'),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: const Text('重要'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ),
+                title: const Text('データ消去'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
